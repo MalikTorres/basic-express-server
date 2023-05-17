05230d991c330cd5e34666fcbbb63361944d0638
Malik Sadiki-Torres <maliktorres98@gmail.com>
2023-05-17 12:41:45 -0400

    updated middleware

--- middleware/validator.js
+++ middleware/validator.js
@@ -3,9 +3,9 @@
 // TODO: write middleware
 // Replace params from demo to query
 module.exports = (req, res, next) => {
-  if(req.query.name === 'Malik'){
+  if(req.query.name){
     next();
   } else {
-    next('The query has to be Malik');
+    next('The query has to be a name');
   }
-}
+};
